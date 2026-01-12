26187c5e24b6d75316f87e2083c8b37794f53e81
Eduart2021 <zeedymarina@yahoo.com>
2026-01-12 14:54:16 -0600

    get random call

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 require('dotenv').config()
+const axios = require('axios');
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
-app.use(express.json)
+// Enabling req.body
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Get requests with axios //
+app.get('/api/fun-fact', async (req,res)=>{
+    try{
+        const getData = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random')
+        res.json(getData.data)
+    }catch(error){
+        console.log(`Error on server request ${error}`);
+        res.status(500).send("Server not responding")
+    }
+})
 
 app.listen(PORT, ()=>{console.log(`Server active on port ${PORT}`)})
